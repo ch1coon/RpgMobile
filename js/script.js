160da90fecc5552e8ca0142fe648da6a2e052a1e
unknown <carlosaraujosantosfc@gmail.com>
2023-05-17 10:45:43 -0300

    vo.3

--- js/script.js
+++ js/script.js
@@ -1,120 +1,136 @@
 function carregarOpcoesRacas(idSelect) {
-    const select = document.getElementById(idSelect);
-  
-    fetch("/data/races.json")
-      .then((response) => response.json())
-      .then((data) => {
-        data.race.forEach((race) => {
+  const select = document.getElementById(idSelect);
+
+  fetch("../data/races.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.race.forEach((race) => {
+        const option = document.createElement("option");
+        option.text = `${race.name} (${race.source})`;
+        select.add(option);
+      });
+    });
+}
+
+function carregarOpcoesSubracas(raceName, idSelect) {
+  const select = document.getElementById(idSelect);
+  select.innerHTML = "";
+
+  fetch("../data/subRaces.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const subraces = data.subrace.filter(
+        (subrace) => subrace.raceName === raceName
+      );
+      if (subraces.length > 0) {
+        subraces.forEach((subrace) => {
           const option = document.createElement("option");
-          option.text = `${race.name} (${race.source})`;
+          option.text = `${subrace.name} (${subrace.source})`;
           select.add(option);
         });
+        select.style.display = "block";
+      } else {
+        select.style.display = "none";
+      }
+    });
+}
+
+function carregarOpcoesBackgrounds(idSelect) {
+  const select = document.getElementById(idSelect);
+
+  fetch("../data/backgrounds.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.background.forEach((background) => {
+        const option = document.createElement("option");
+        option.text = `${background.name} (${background.source})`;
+        select.add(option);
       });
-  }
-  
-  function carregarOpcoesSubracas(raceName, idSelect) {
-    const select = document.getElementById(idSelect);
-    select.innerHTML = "";
-  
-    fetch("/data/subRaces.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const subraces = data.subrace.filter(
-          (subrace) => subrace.raceName === raceName
-        );
-        if (subraces.length > 0) {
-          subraces.forEach((subrace) => {
-            const option = document.createElement("option");
-            option.text = `${subrace.name} (${subrace.source})`;
-            select.add(option);
-          });
-          select.style.display = "block";
-        } else {
-          select.style.display = "none";
-        }
-      });
-  }
-  
-  function carregarOpcoesBackgrounds(idSelect) {
-    const select = document.getElementById(idSelect);
-  
-    fetch("/data/backgrounds.json")
-      .then((response) => response.json())
-      .then((data) => {
-        data.background.forEach((background) => {
-          const option = document.createElement("option");
-          option.text = `${background.name} (${background.source})`;
-          select.add(option);
-        });
-      });
-  }
-  
-  function atualizarAtributos(raceName) {
-    fetch("/data/races.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const race = data.race.find((race) => race.name === raceName);
-  
+    });
+}
+
+function atualizarAtributos(raceName, subraceName) {
+  fetch("../data/races.json")
+    .then((response) => response.json())
+    .then((data) => {
+      let race;
+
+      if (Array.isArray(data) && data.length > 0) {
+        const filteredData = data.filter((race) => race.name === raceName);
+        race = filteredData[0];
+
         if (race) {
-          const bonusTextDiv = document.getElementById("bonus-text");
-          let bonusText = "";
-  
-          if (race.ability && race.ability.choose) {
-            bonusText += "Escolha entre: ";
-  
-            // Remove os botões existentes antes de adicionar os novos
-            bonusTextDiv.innerHTML = "";
-  
-            race.ability.choose.forEach((attribute) => {
-              const value = race.ability[attribute];
-              if (value !== undefined) {
-                const button = document.createElement("button");
-                button.textContent = `${attribute} + ${value}`;
-                button.addEventListener("click", () => {
-                  bonusTextDiv.textContent = `${attribute} + ${value}`;
-                });
-                bonusTextDiv.appendChild(button);
-              }
-            });
-          } else {
-            for (const attribute in race.ability) {
-              const value = race.ability[attribute];
-              if (value !== undefined && value !== 0) {
-                bonusText += `${attribute} + ${value} `;
-              }
+          let attributes = {};
+
+          if (subraceName) {
+            const subrace = race.subrace.find((subrace) => subrace.name === subraceName);
+            if (subrace && subrace.ability) {
+              attributes = { ...race.ability, ...subrace.ability };
+            } else {
+              attributes = race.ability;
             }
-  
-            bonusTextDiv.textContent = bonusText.trim();
+          } else {
+            attributes = race.ability;
+          }
+
+          if (attributes && Object.keys(attributes).length > 0) {
+            const bonusTextDiv = document.getElementById("bonus-text");
+            bonusTextDiv.innerHTML = "";
+          
+            if (attributes.choose) {
+              const { choose, ...remainingAttributes } = attributes;
+              const chooseOptions = choose.from.map((option) => {
+                const value = remainingAttributes[option];
+                return `${option} + ${value}`;
+              });
+              bonusTextDiv.textContent = `Escolha entre: ${chooseOptions.join(", ")}`;
+            } else {
+              const bonusText = Object.entries(attributes)
+                .filter(([_, value]) => value !== 0)
+                .map(([attribute, value]) => `${attribute} + ${value}`)
+                .join(" ");
+              bonusTextDiv.textContent = bonusText;
+            }
+          } else {
+            const bonusTextDiv = document.getElementById("bonus-text");
+            bonusTextDiv.textContent = "Atributos não encontrados";
           }
         } else {
-          const bonusTextDiv = document.getElementById("bonus-text");
-          bonusTextDiv.textContent = "Raça inválida";
+          console.error("Raça não encontrada no arquivo JSON");
         }
-      })
-      .catch((error) => {
-        console.error("Erro ao carregar o JSON races.json:", error);
-      });
-  }
-  
-  
-  
-  
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const raceSelect = document.getElementById("race-select");
-    const subraceSelect = document.getElementById("subrace-select");
-    const backgroundSelect = document.getElementById("background-select");
-  
-    raceSelect.addEventListener("change", () => {
-      const raceName = raceSelect.value.split(" (")[0];
-      carregarOpcoesSubracas(raceName, "subrace-select");
-      atualizarAtributos(raceName);
+      } else {
+        console.error("O arquivo JSON não contém dados válidos");
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar o JSON races.json:", error);
     });
-  
-    carregarOpcoesRacas("race-select");
-    carregarOpcoesSubracas(raceSelect.value.split(" (")[0], "subrace-select");
-    carregarOpcoesBackgrounds("background-select");
-  
-    atualizarAtributos(raceSelect.value.split(" (")[0]);
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const raceSelect = document.getElementById("race-select");
+  const subraceSelect = document.getElementById("subrace-select");
+  const backgroundSelect = document.getElementById("background-select");
+
+  raceSelect.addEventListener("change", () => {
+    const raceName = raceSelect.value.split(" (")[0];
+    carregarOpcoesSubracas(raceName, "subrace-select");
+    if (raceName) {
+      atualizarAtributos(raceName);
+    } else {
+      const bonusTextDiv = document.getElementById("bonus-text");
+      bonusTextDiv.textContent = "";
+    }
   });
-  
+
+  carregarOpcoesRacas("race-select");
+  carregarOpcoesSubracas(raceSelect.value.split(" (")[0], "subrace-select");
+  carregarOpcoesBackgrounds("background-select");
+
+  const initialRaceName = raceSelect.value.split(" (")[0];
+  if (initialRaceName) {
+    atualizarAtributos(initialRaceName);
+  }
+});
